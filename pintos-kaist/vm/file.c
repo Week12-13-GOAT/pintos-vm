1aa4755903a9d101675a9e65290f744abf64ef3c
hmmwtf <kjh91207@gmail.com>
2025-06-02 14:28:58 +0000

    [FEAT] file swap in/out 구현
    Message body
    in file.c -> swap in/out, destory 까지 구현 완 -> 확인 필요함
    
    Message footer
    PintOS Project 3 : VM

--- pintos-kaist/vm/file.c
+++ pintos-kaist/vm/file.c
@@ -4,6 +4,9 @@
 #include "userprog/process.h"
 #include "threads/vaddr.h"
 #include "threads/mmu.h"
+#include "string.h"
+
+struct lock filesys_lock;
 
 static bool file_backed_swap_in(struct page *page, void *kva);
 static bool file_backed_swap_out(struct page *page);
@@ -56,13 +59,26 @@ file_backed_swap_in(struct page *page, void *kva)
 {
 	struct file_page *file_page UNUSED = &page->file;
 	// swap_in을 위한 버퍼
-	void *buffer[PGSIZE];
+	// void *buffer[PGSIZE];
 	/** TODO: 파일에서 정보를 읽어와 kva에 복사하세요
 	 * aux에 저장된 백업 정보를 사용하세요
 	 * file_open과 read를 사용하면 될 것 같아요
 	 * 파일 시스템 동기화가 필요할수도 있어요
 	 * 필요시 file_backed_initializer를 수정하세요
 	 */
+	struct file *file = file_page->file;
+	off_t offset = file_page->offset;
+	size_t read_byte = file_page->read_byte;
+
+	// 파일에서 데이터 읽기
+	if (file_read_at(file, kva, read_byte, offset) != (off_t)read_byte) {
+		return false;
+	}
+
+	// 파일에서 읽어오지 못한 나머지 페이지 영역을 0으로 초기화
+	memset(kva + read_byte, 0, page->file.zero_byte);
+
+	return true;
 }
 
 /* 페이지의 내용을 파일에 기록(writeback)하여 스왑아웃합니다. */
@@ -76,6 +92,32 @@ file_backed_swap_out(struct page *page)
 	 * file_write를 사용하면 될 것 같아요
 	 * dirty_bit 초기화 (pml4_set_dirty)
 	 */
+	struct thread *curr = thread_current();
+	bool dirty_bit = pml4_is_dirty(curr->pml4, page->va);
+
+	if (dirty_bit == true) {
+		lock_acquire(&filesys_lock);
+		if (file_write_at(file_page->file, page->frame->kva, file_page->read_byte, file_page->offset)
+			!= (off_t)file_page->read_byte) {
+				lock_release(&filesys_lock);
+				return false;
+		}
+		lock_release(&filesys_lock);
+
+		// 더티 비트 클리어
+		pml4_set_dirty(curr->pml4, page->va, false);
+	}
+
+	// 페이지 테이블 매핑 제거
+	pml4_clear_page(curr->pml4, page->va);
+
+	// 프레임 해제
+	if (page->frame != NULL) {
+		free(page->frame);
+		page->frame = NULL;
+	}
+
+	return true;
 }
 
 /* 파일 기반 페이지를 소멸시킵니다. PAGE는 호출자가 해제합니다. */
@@ -88,13 +130,25 @@ file_backed_destroy(struct page *page)
 	 * write back을 할 때는 aux에 저장된 파일 정보를 사용
 	 * file_write를 사용하면 될 것 같아요
 	 */
+	struct thread *curr = thread_current();
 	if (pml4_is_dirty(thread_current()->pml4, page->va))
 	{
+		lock_acquire(&filesys_lock);
 		file_write_at(file_page->file, page->frame->kva, file_page->read_byte, file_page->offset);
+		lock_release(&filesys_lock);
 	}
+
+	file_allow_write(file_page->file);
+
+	if (page->frame != NULL) {
+		free(page->frame);
+		page->frame = NULL;
+	}
+
+	pml4_clear_page(curr->pml4, page->va);
 }
 
-struct lazy_load_info *make_info(
+static struct lazy_load_info *make_info(
 	struct file *file, off_t offset, size_t read_byte)
 {
 	struct lazy_load_info *info = malloc(sizeof(struct lazy_load_info));
@@ -105,7 +159,7 @@ struct lazy_load_info *make_info(
 	return info;
 }
 
-struct mmap_info *make_mmap_info(struct lazy_load_info *info, int mapping_count)
+static struct mmap_info *make_mmap_info(struct lazy_load_info *info, int mapping_count)
 {
 	struct mmap_info *mmap_info = malloc(sizeof(struct mmap_info));
 	mmap_info->info = info;
@@ -142,7 +196,7 @@ void *do_mmap(void *addr, size_t length, int writable,
 		struct mmap_info *mmap_info = make_mmap_info(info, mapping_count);
 		void *aux = mmap_info;
 
-		vm_alloc_page_with_initializer(VM_MMAP, cur_addr, writable, lazy_load_segment, aux);
+		vm_alloc_page_with_initializer(VM_MMAP, addr, writable, lazy_load_segment, aux);
 		if (remain_length < PGSIZE)
 			break;
 		remain_length -= PGSIZE;
